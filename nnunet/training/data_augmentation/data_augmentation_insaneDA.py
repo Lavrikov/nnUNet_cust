10adf78e9a8820d0c3190264c08a46237e228faa
A.Lavrikov <32636269+Lavrikov@users.noreply.github.com>
2023-09-19 03:25:18 +0400

    Update data_augmentation_insaneDA.py

--- nnunet/training/data_augmentation/data_augmentation_insaneDA.py
+++ nnunet/training/data_augmentation/data_augmentation_insaneDA.py
@@ -12,9 +12,15 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from batchgenerators.dataloading import MultiThreadedAugmenter
-from batchgenerators.transforms import DataChannelSelectionTransform, SegChannelSelectionTransform, SpatialTransform, \
-    GammaTransform, MirrorTransform, Compose
+#from batchgenerators.dataloading import MultiThreadedAugmenter
+#from batchgenerators.transforms import DataChannelSelectionTransform, SegChannelSelectionTransform, SpatialTransform, \
+#    GammaTransform, MirrorTransform, Compose
+
+from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter  
+from batchgenerators.dataloading.single_threaded_augmenter import SingleThreadedAugmenter
+from batchgenerators.transforms.channel_selection_transforms import DataChannelSelectionTransform, \
+    SegChannelSelectionTransform
+
 from batchgenerators.transforms.color_transforms import BrightnessMultiplicativeTransform, \
     ContrastAugmentationTransform, BrightnessTransform
 from batchgenerators.transforms.noise_transforms import GaussianNoiseTransform, GaussianBlurTransform
