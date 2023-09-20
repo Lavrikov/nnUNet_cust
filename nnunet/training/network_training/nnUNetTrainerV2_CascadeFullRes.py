89538a42d674269ec6fb1ae902951c59eb04d64b
A.Lavrikov <32636269+Lavrikov@users.noreply.github.com>
2023-09-20 19:57:28 +0400

    Update nnUNetTrainerV2_CascadeFullRes.py

--- nnunet/training/network_training/nnUNetTrainerV2_CascadeFullRes.py
+++ nnunet/training/network_training/nnUNetTrainerV2_CascadeFullRes.py
@@ -18,7 +18,7 @@ from time import sleep
 import matplotlib
 from nnunet.configuration import default_num_threads
 from nnunet.postprocessing.connected_components import determine_postprocessing
-from nnunet.training.data_augmentation.data_augmentation_moreDA import get_moreDA_augmentation
+#from nnunet.training.data_augmentation.data_augmentation_moreDA import get_moreDA_augmentation
 from nnunet.training.dataloading.dataset_loading import DataLoader3D, unpack_dataset
 from nnunet.evaluation.evaluator import aggregate_scores
 from nnunet.network_architecture.neural_network import SegmentationNetwork
@@ -165,12 +165,12 @@ class nnUNetTrainerV2CascadeFullRes(nnUNetTrainerV2):
                         "INFO: Not unpacking data! Training may be slow due to that. Pray you are not using 2d or you "
                         "will wait all winter for your model to finish!")
 
-                self.tr_gen, self.val_gen = get_moreDA_augmentation(self.dl_tr, self.dl_val,
-                                                                    self.data_aug_params[
-                                                                        'patch_size_for_spatialtransform'],
-                                                                    self.data_aug_params,
-                                                                    deep_supervision_scales=self.deep_supervision_scales,
-                                                                    pin_memory=self.pin_memory)
+                # self.tr_gen, self.val_gen = get_moreDA_augmentation(self.dl_tr, self.dl_val,
+                #                                                     self.data_aug_params[
+                #                                                         'patch_size_for_spatialtransform'],
+                #                                                     self.data_aug_params,
+                #                                                     deep_supervision_scales=self.deep_supervision_scales,
+                #                                                     pin_memory=self.pin_memory)
                 self.print_to_log_file("TRAINING KEYS:\n %s" % (str(self.dataset_tr.keys())),
                                        also_print_to_console=False)
                 self.print_to_log_file("VALIDATION KEYS:\n %s" % (str(self.dataset_val.keys())),
